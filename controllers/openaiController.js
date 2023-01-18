752d44d0c5cbd9e8c34cd2d41f0d5fed835f9ab1
NazmulHossainShovon <shovon2228@gmail.com>
2023-01-18 22:11:56 +0600

    deleted unused console logs

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -22,13 +22,6 @@ const generateImage = async (req, res) => {
       data: imageUrl,
     });
   } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
-    }
-
     res.status(400).json({
       success: false,
       error: "The Image could not be generated",
